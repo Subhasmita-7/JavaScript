f2d9351231ed8adf3f16921a5bf1432285c27c2a
subhasmita <155456481+Subhasmita-7@users.noreply.github.com>
2025-01-23 12:25:27 +0000

    variables

---
+++ 1basics/variables.js
@@ -0,0 +1,18 @@
+let a = 17
+var b 
+const c = 5
+
+a=1
+//b=2  when you dont assign a value, you will get undefined
+
+//c=3  you cannot re-assign to a const variable 
+
+// using var is not preferred that much because whenever you create any file with the same variable name then the value will be 
+// changed in every file where the same variable name exists.
+
+// Use let and avoid using any name without variable
+console.log("a,b,c: ",a,b,c)
+b=2
+console.table([a,b,c])
+
+console.log(typeof b)
